44b7b31332d74535241dc4b5bd38fe43baa18fea
Talex210 <tal.89@mail.ru>
2025-09-11 18:13:56 +0300

    Fixed load app

--- jira-project-assistant/static/ui/vite.config.js
+++ jira-project-assistant/static/ui/vite.config.js
@@ -5,11 +5,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+    assetsDir: '',
     rollupOptions: {
-      input: './index.html'
+      input: './index.html',
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
     }
   },
   define: {
     global: 'globalThis',
-  }
+  },
+  base: './'
 });
